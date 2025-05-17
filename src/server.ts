46c94a1000a90088fdbf220bd3207a3a62b0c43d
Matheus <mateheusoliver@gmail.com>
2025-05-17 18:37:26 -0300

    adjusting server to include migration ( setup )

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app";
 import { connectToMongo } from "./configs/mongoClient";
+import { setupIndexes } from "./configs/setup";
 
 dotenv.config();
 
@@ -9,11 +10,13 @@ const port = process.env.PORT || 3000;
 async function startServer() {
     try {
         await connectToMongo();
+        await setupIndexes();
+
         app.listen(port, () => {
-            console.log(`Server running on port:${port}`);
+            console.log(`🚀 Server running at http://localhost:${port}`);
         });
     } catch (error) {
-        console.error("error starting server", error);
+        console.error("❌ error starting server:", error);
         process.exit(1);
     }
 }
